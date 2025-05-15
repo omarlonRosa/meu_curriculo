775564a30d1804f0f40fd54455918cff158babe9
Marlon Rosa <rosa.marlon@icloud.com>
2025-05-15 00:55:51 -0300

    iniciando deploy do curriculo

---
+++ js/script.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', typeWriter);
+
+function typeWriter() {
+  const text = 'Marlon Franca Rosa';
+  let index = 0;
+  const nameElement = document.getElementById('name');
+
+  const typingEffect = setInterval(() => {
+    nameElement.textContent = text.slice(0, ++index) + '|';
+    if (index === text.length) clearInterval(typingEffect);
+  }, 100);
+
+  if(localStorage.getItem('theme') === 'dark') {
+    document.documentElement.classList.add('dark');
+  }
+
+}
+
+function shareLink() {
+
+  const link = window.location.href;
+  const phoneNumber = '+559499202-5613';
+  const message = `👋 Olá! Sou Marlon Rosa, Engenheiro de Software e Designer Gráfico. 
+Tenho experiência com desenvolvimento em React, NextJS, JavaScript e design gráfico (Photoshop e Illustrator).
+💡 Atualmente estou + trabalhando em projetos com Phoenix e Elixir. 
+📌 Vamos conversar? 
+Você pode me contatar diretamente aqui: ${phoneNumber}.
+Confira meu currículo completo aqui: ${encodeURIComponent(link)}`;
+
+
+  const encodedMessage = encodeURIComponent(message);
+
+  const whatsappUrl = `https://api.whatsapp.com/send?text=${encodedMessage}`;
+  window.open(whatsappUrl, '_blank');
+}
+
+
+function downloadPDF() {
+  const element = document.getElementById("curriculo");
+
+  if (element) {
+    html2pdf().from(element).save('Curriculo_Marlon_Rosa.pdf');
+  } else {
+    console.error("Currículo não encontrado!");
+    alert("Erro: Currículo não encontrado!");
+  }
+}
